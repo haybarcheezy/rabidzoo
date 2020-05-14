f46f4e78a3750d36225f5c22366290f5f0bcd1af
Hayden <30369975+haybarcheezy@users.noreply.github.com>
2020-05-14 18:16:28 -0500

    removed

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 let express = require('express');
-const cool = require('cool-ascii-faces');
-let bodyParser = require('body-parser');
-const path = require('path');
-const companion = require('@uppy/companion');
-const PORT = process.env.PORT || 3000;
+let path = require('path');
+let PORT = process.env.PORT || 3000;
+
+
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (req, res) => res.render('pages/index.html'))
-	.use(bodyParser.json())
-	.use(session({ secret: 'some secrety secret' }))
-	.get('/cool', (req, res) => res.send(cool()))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+	
